66b0efc092b3eaf2fb1b30e94e208c29f4bf3d13
xhosad00 <xhosad00@stud.fit.vutbr.cz>
2024-04-23 20:02:18 +0200

    Added_doxy_comments

--- discreteSim.cpp
+++ discreteSim.cpp
@@ -8,21 +8,63 @@
 
 #include "discreteSim.hpp"
 
-const int IgnoreID = -1;
-unsigned int SEED = 5; //random number from Hardware
+unsigned int SEED = 0; 
 // TODo parse from args? gen seed at random
 
 // namespace discSim
 // {
 
-    double uniformDis(double a, double b)
-    {
-        static std::default_random_engine gen(SEED);
-        std::uniform_real_distribution<double> dis(a, b);
+/**********DISTRIBUTIONS**********/
 
-        return dis(gen);
+
+    /**
+     * @brief Parse seed from arguments if it was set
+     * 
+     * @param argc 
+     * @param argv 
+     * @param seed Teference to variable where seed will be stored
+     * @return 0 if valid arguments / none oarguments ; return 1 if incorrect format
+     */
+    int parseArguments(int argc, char *argv[], unsigned int &seed) 
+    {
+        if (argc > 1) {
+            std::string arg = argv[1];         
+            if (arg == "--seed") {
+                if (argc > 2) {
+                    seed = std::atoi(argv[2]);
+                    return 0; 
+                } else {
+                    std::cerr << "No seed value provided." << std::endl;
+                    return 1; 
+                }
+            } else {
+                std::cerr << "Invalid argument. Use '--seed' <int>" << std::endl;
+                return 1; 
+            }
+        }
+        return 0;
     }
 
+    /**
+     * @brief Generates a random number from a uniform distribution between a and b
+     * 
+     * @param a The lower bound of the uniform distribution
+     * @param b The upper bound of the uniform distribution
+     * @return A random number from the uniform distribution
+     */
+double uniformDis(double a, double b)
+{
+    static std::default_random_engine gen(SEED);
+    std::uniform_real_distribution<double> dis(a, b);
+
+    return dis(gen);
+    }
+    /**
+     * @brief Generates a random number from an exponential distribution with parameter lambda
+     * 
+     * @param lambd The rate parameter of the exponential distribution
+     * @return A random number from the exponential distribution
+     */
     double expDis(double lambd)
     {
         static std::default_random_engine gen(SEED);
@@ -30,14 +72,23 @@ unsigned int SEED = 5; //random number from Hardware
 
         return dis(gen);
     }
-
+    /**
+     * @brief Generates a random number from a normal distribution with specified mean and standard deviation
+     * 
+     * @param mean The mean of the normal distribution
+     * @param stddev The standard deviation of the normal distribution
+     * @return A random number from the normal distribution
+     */
     double normalDis(double mean, double stddev)
     {
         static std::default_random_engine gen(SEED);
         std::normal_distribution<double> dis(mean, stddev);
         return dis(gen);
     }
+/**********DISTRIBUTIONS**********/
 
+
+/**********EVENT**********/
     /**
      * @brief Construct a new Event:: Event object
      * 
@@ -49,6 +100,9 @@ unsigned int SEED = 5; //random number from Hardware
      * @param created 
      */
     Event::Event(int proc, int next, int fac, double start, int prio, double created) : processID(proc), processNextState(next), facilityID(fac), startTime(start), priority(prio), timeCreated(created){};
+    /**
+     * @brief Default constructor for Event class
+     */
     Event::Event() // create an empty event
     {
         processID = IgnoreID;
@@ -60,7 +114,7 @@ unsigned int SEED = 5; //random number from Hardware
     }
 
     /**
-     * @brief if can process event 
+     * @brief check if simulation can process this event 
      * 
      * @return true if is process or facility event
      * @return false if custom event
@@ -70,16 +124,29 @@ unsigned int SEED = 5; //random number from Hardware
         return this->isProcessEvent() || this->isFacilityEvent();
     }
 
+    /**
+     * @brief Checks if the event is a process event
+     * 
+     * @return true if the event is a process event, false otherwise
+     */
     bool Event::isProcessEvent()
     {
         return processID != IgnoreID && facilityID == IgnoreID;
     }
 
+    /**
+     * @brief Checks if the event is a facility event
+     * 
+     * @return true if the event is a facility event, false otherwise
+     */
     bool Event::isFacilityEvent()
     {
         return facilityID != IgnoreID;
     }
+/**********EVENT**********/
 
+
+/**********PROCESS**********/
     /**
      * @brief Construct a new Process:: Process object
      * 
@@ -135,7 +202,10 @@ unsigned int SEED = 5; //random number from Hardware
     {
         this->sim->seizeFacility(this->id, nextState, facID, prio);
     }
+/**********PROCESS**********/
 
+
+/**********FACILITY**********/
     /**
      * @brief Construct a new Facility:: Facility object
      * 
@@ -155,12 +225,23 @@ unsigned int SEED = 5; //random number from Hardware
             throw std::invalid_argument("Uniform distribution attribute 'a' cannot be less than 'b'");
     }
 
+    /**
+     * @brief Get the ID of the facility
+     * 
+     * @return The ID of the facility
+     */
     int Facility::getId()
     {
         return this->id;
     }
 
-    // override print funciton
+    /**
+     * @brief Overloaded stream insertion operator for printing Facility objects
+     * 
+     * @param os The output stream
+     * @param f The Facility object to be printed
+     * @return The output stream
+     */
     std::ostream &operator<<(std::ostream &os, const Facility &f) 
     {
         os << "Facility: " << f.name << "\n  processCnt   :" << f.stats.processCnt << "\n  waitTimeTotal:" << f.stats.waitTimeTotal << "\n  workTimeTotal:" << f.stats.workTimeTotal << "\n";
@@ -172,7 +253,7 @@ unsigned int SEED = 5; //random number from Hardware
     /**
      * @brief generatime time value based on facilitys GenType and gen values (a,b)
      * 
-     * @return double 
+     * @return The generated time value 
      */
     double Facility::generateTime()
     {
@@ -210,9 +291,14 @@ unsigned int SEED = 5; //random number from Hardware
         this->stats.workTimeTotal += time;
     };
 
+    /**
+     * @brief Handle process exit from the facility
+     * 
+     * @param proc Pointer to the process exiting the facility
+     * @param e The event triggering the process exit
+     */
     void Facility::ProcessExit(Process *proc, Event e)
     {
-        //TODO stats
         proc->state = e.processNextState;
         proc->doBehavior();
         if (this->q.empty())
@@ -229,6 +315,9 @@ unsigned int SEED = 5; //random number from Hardware
         }
     }
 
+    /**
+     * @brief Print statistics related to the facility
+     */
     void Facility::printStats()
     {
         printf("%2d: %s\n", id, name.c_str());
@@ -237,6 +326,14 @@ unsigned int SEED = 5; //random number from Hardware
         printf("  wait time total: %.3lf\n", stats.waitTimeTotal);
     }
 
+/**********FACILITY**********/
+
+
+
+/**********SIMULATION**********/
+    /**
+     * @brief Default constructor for Simulation class
+     */
     Simulation::Simulation()
     {
         time = 0;
@@ -244,40 +341,88 @@ unsigned int SEED = 5; //random number from Hardware
         // sharedThis = std::shared_ptr<Simulation>(this);
     }
 
+    /**
+     * @brief Get the current simulation time
+     * 
+     * @return The current simulation time
+     */
     double Simulation::getTime()
     // double Simulation::getTime()
     {
         return this->time;
     }
 
+    /**
+     * @brief Set the end time of the simulation
+     * 
+     * @param time The end time of the simulation
+     */
     void Simulation::setEndTime(double time)
     {
         this->endTime = time;
     }
 
-    // void Simulation::addEvent(int processID, int processNextState, int facilityID, double startTime, int priority, double timeCreated)
-    // {
-    //     // Event e = Event(processID, processNextState, facilityID, startTime, priority, timeCreated);
-    //     calendar.emplace(processID, processNextState, facilityID, startTime, priority, timeCreated);
-    // }
+    /**
+     * @brief Add an event to the simulation
+     * 
+     * @param processID The ID of the process associated with the event
+     * @param processNextState The next state of the associated process
+     * @param facilityID The ID of the facility associated with the event
+     * @param startTime The start time of the event
+     * @param priority The priority of the event. Higher priority is better (100 is important, 0 is less)
+     * @param timeCreated The time when the event was created
+     */
+    void Simulation::addEvent(int processID, int processNextState, int facilityID, double startTime, int priority, double timeCreated)
+    {
+        calendar.emplace(processID, processNextState, facilityID, startTime, priority, timeCreated);
+    }
 
-    // void Simulation::addEvent(Event e)
-    // {
-    //     calendar.push(e);
-    // }
+    /**
+     * @brief Add an event to the simulation
+     * 
+     * @param e The event to be added
+     */
+    void Simulation::addEvent(Event e)
+    {
+        calendar.push(e);
+    }
 
+    /**
+     * @brief Add a process event to the simulation
+     * 
+     * @param processID The ID of the process associated with the event
+     * @param processNextState The next state of the associated process
+     * @param startTime The start time of the event
+     * @param priority The priority of the event. Higher priority is better (100 is important, 0 is less)
+     * @param timeCreated The time when the event was created
+     */
     void Simulation::addProcessEvent(int processID, int processNextState, double startTime, int priority, double timeCreated)
     {
         Event e = Event(processID, processNextState, IgnoreID, startTime, priority, this->time);
         calendar.push(e);
     }
 
+    /**
+     * @brief Add a facility event to the simulation
+     * 
+     * @param processID The ID of the process associated with the event
+     * @param processNextState The next state of the associated process
+     * @param facilityID The ID of the facility associated with the event
+     * @param startTime The start time of the event
+     * @param priority The priority of the event. Higher priority is better (100 is important, 0 is less)
+     * @param timeCreated The time when the event was created
+     */
     void Simulation::addFacilityEvent(int processID, int processNextState, int facilityID, double startTime, int priority, double timeCreated)
     {
         Event e = Event(processID, processNextState, facilityID, startTime, priority, timeCreated);
         calendar.push(e);
     }
 
+    /**
+     * @brief Get the next event from the event calendar.
+     * 
+     * @return The next event from the event calendar.
+     */
     Event Simulation::nextEvent()
     {
         Event e = calendar.top();
@@ -286,6 +431,11 @@ unsigned int SEED = 5; //random number from Hardware
         return e;
     }
 
+    /**
+     * @brief Check if the simulation has finished.
+     * 
+     * @return true if the simulation has finished, false otherwise.
+     */
     bool Simulation::finished()
     {
         return calendar.empty() || (this->endTime > 0 && this->time > this->endTime);
@@ -343,7 +493,12 @@ unsigned int SEED = 5; //random number from Hardware
         return true;
     }
 
-
+    /**
+     * @brief Execute an event in the simulation
+     * 
+     * @param e The event to be executed
+     * @return Pointer to the executed event
+     */
     Event* Simulation::executeEvent(Event e)
     {
         if (e.isProcessEvent())
@@ -389,6 +544,13 @@ unsigned int SEED = 5; //random number from Hardware
         return new Event(e); // TODo check
     }
 
+    /**
+     * @brief Activate a process in the simulation at current sim time
+     * 
+     * @param processID The ID of the process to be activated
+     * @param state The state to which the process should transition after activation
+     * @param prio Priority of the process activation
+     */
     void Simulation::activate(int processID, int state, int prio)
     {
         std::unordered_map<int, Process>::const_iterator i = procMap.find(processID);
@@ -400,6 +562,14 @@ unsigned int SEED = 5; //random number from Hardware
         calendar.emplace(p.id, state, IgnoreID, this->time, prio, this->time); 
     }
 
+    /**
+     * @brief Make a process wait for a specified time in the simulation
+     * 
+     * @param processID The ID of the process to wait
+     * @param state The state to which the process should transition after waiting
+     * @param delay The time to wait
+     * @param prio Priority of the process waiting
+     */
     void Simulation::waitFor(int processID, int state, double delay, int prio)
     {        
         std::unordered_map<int, Process>::const_iterator i = procMap.find(processID);
@@ -411,6 +581,14 @@ unsigned int SEED = 5; //random number from Hardware
         calendar.emplace(p.id, state, IgnoreID, this->time + delay, prio, this->time);         
     }
 
+    /**
+     * @brief Seize a facility by a process in the simulation
+     * 
+     * @param processID The ID of the process seizing the facility
+     * @param state The state to which the process should transition after seizing the facility
+     * @param facilityID The ID of the facility to be seized
+     * @param prio Priority of the process seizing the facility
+     */
     void Simulation::seizeFacility(int processID, int state, int facilityID, int prio)
     {
         std::unordered_map<int, Facility>::iterator fi = facMap.find(facilityID); // TODO switch to find
@@ -448,6 +626,11 @@ unsigned int SEED = 5; //random number from Hardware
         }
     }
 
+    /**
+     * @brief Create a facility and add it to the simulation
+     * 
+     * @param f The facility to be created
+     */
     void Simulation::createFacility(Facility f)
     {
         // faci.emplace(p.id, state, IgnoreID, this->time, prio, this->time); 
@@ -469,6 +652,12 @@ unsigned int SEED = 5; //random number from Hardware
         this->facMap.emplace(id, Facility(id, n, cap, g, a, b));
     }
 
+    /**
+     * @brief Find a facility by its ID in the simulation
+     * 
+     * @param id The ID of the facility to find
+     * @return Pointer to the facility if found, nullptr otherwise
+     */
     Facility *Simulation::findFacility(int id)
     {
         std::unordered_map<int, Facility>::iterator fi = facMap.find(id); 
@@ -480,6 +669,9 @@ unsigned int SEED = 5; //random number from Hardware
         return &fi->second;
     }
 
+    /**
+     * @brief Print statistics of all facilities in the simulation
+     */
     void Simulation::printFacilitysStats()
     {
         printf("\n----PRINT FACILITY STATS----\n");
--- discreteSim.hpp
+++ discreteSim.hpp
@@ -18,16 +18,18 @@
 #include <stdexcept>
 
 
-extern const bool Verbose; // Declare the extern variable
+extern const bool Verbose;  ///< External boolean variable controlling verbose output
+const int IgnoreID = -1;    ///< Constant indicating an ID to be ignored, used for procID and facID
+extern unsigned int SEED;      ///< Seed for random number
 // namespace discSim 
 // {
     
-    const int CREATE_PROCESS_PRIO = 60;
-    const int ACTIVATE_PROCESS_PRIO = 50;
-    const int SEIZE_FACILITY_PRIO = 20;
-    const int EXIT_FACILITY_PRIO = 30;
+    const int CREATE_PROCESS_PRIO = 60;     ///< Priority for creating a process
+    const int ACTIVATE_PROCESS_PRIO = 50;   ///< Priority for activating a process
+    const int SEIZE_FACILITY_PRIO = 20;     ///< Priority for seizing a facility
+    const int EXIT_FACILITY_PRIO = 30;      ///< Priority for exiting a facility
 
-    
+    int parseArguments(int argc, char* argv[], unsigned int& seed);
     double uniformDis(double a, double b);
     double expDis(double lambd);
     double normalDis(double mean, double stddev);
@@ -35,19 +37,33 @@ extern const bool Verbose; // Declare the extern variable
     class Simulation;
 
 
+    /**
+     * @brief The Event class represents an event in the discrete event simulation
+     * 
+     * Events are scheduled to occur at specific times and are associated with
+     * processes, facilities, or other entities in the simulation
+     */
     class Event
     {
     public:
-        int processID;
-        int processNextState;
-        int facilityID; //TODO how will facility be handled?
-        double startTime;
-        int priority;       //As of now higher priority is better (100 important, 0 less)
-        double timeCreated;
+        int processID;          ///< The ID of the process associated with the event
+        int processNextState;   ///< The next state of the associated process
+        int facilityID;         ///< The ID of the facility associated with the event
+        double startTime;       ///< The start time of the event
+        int priority;           ///< The priority of the event. Higher priority is better. (100 is important, 0 is less)
+        double timeCreated;     ///< The time when the event was created
 
         Event(int proc, int nxt, int fac, double start, int prio, double created);
         Event();
 
+        /**
+         * @brief Overloaded less-than operator for comparing events
+         * 
+         * Events are compared based on their start time, priority, and creation time
+         * 
+         * @param other The event to compare with
+         * @return true if this event is scheduled to occur earlier than the other event, false otherwise
+         */
         bool operator<(const Event& other) const {
             if (startTime != other.startTime)
                 return startTime > other.startTime;
@@ -61,16 +77,21 @@ extern const bool Verbose; // Declare the extern variable
         bool isFacilityEvent();
     };
 
+    /**
+     * @brief The Process class represents a discrete process in the simulation
+     * 
+     * Processes are entities that perform actions over time in the simulation
+     */
     class Process
     {
     public:
-        int id;
-        int state = 0;
-        void* data;
-        void (*behav)(Process*, void* data);
-        Simulation* sim;
-        void* buffer;
-
+        int id;                     ///< The ID of the process
+        int state;                  ///< The current state of the process
+        void* data;                 ///< Pointer to additional data associated with the process
+        void (*behav)(Process*, void*);  ///< Pointer to the behavior function of the process
+        Simulation* sim;            ///< Pointer to the Simulation object associated with the process
+        void* buffer;               ///< Pointer to a buffer used by the process (if any)
+        
         Process(int st, void (*b)(Process*, void*), Simulation* sm = nullptr, void* data = nullptr);
 
         void setBehavior(void (*function)(Process*, void*));
@@ -78,12 +99,23 @@ extern const bool Verbose; // Declare the extern variable
         void seize(int facID, int nextState, int prio = SEIZE_FACILITY_PRIO);
     };
 
-
+    /**
+     * @brief The Facility class represents a facility in the discrete event simulation
+     * 
+     * Facilities are resources that can be seized by processes to perform activities
+     */
     class Facility
     {
     public:
-        enum class GenType {Uniform, Exp, Normal};
-        // Facility(std::string n = "", int cap = 1, GenType g = GenType::Uniform, double a = 0, double b = 0);
+        /**
+         * @brief This enum represents the types of distributions used for generating random numbers
+         * 
+         */
+        enum class GenType {
+            Uniform,    ///< Uniform distribution
+            Exp,        ///< Exponential distribution
+            Normal      ///< Normal distribution
+        };
         
         Facility(int id, std::string n, int cap, GenType g , double a , double );
         friend std::ostream& operator<<(std::ostream& os, const Facility& f);
@@ -94,44 +126,46 @@ extern const bool Verbose; // Declare the extern variable
         void printStats();
         
     
-        struct FacilityStats
+        struct FacilityStats    ///< Structure to hold facility statistics
         {
-            int processCnt;
-            double waitTimeTotal;
-            double workTimeTotal;
+            int processCnt;         ///< The count of processes using the facility
+            double waitTimeTotal;   ///< The total wait time of processes at the facility
+            double workTimeTotal;   ///< The total work time of the facility
         };
-        struct ProcInQueue
+        struct ProcInQueue  ///< Structure to hold information about a process in the facility queue
         {
-            Process* p;
-            int processNextState;
-            double enteredQueueTime;
+            Process* p;             ///< Pointer to the process
+            int processNextState;   ///< The next state of the process
+            double enteredQueueTime;    ///< The time when the process entered the queue
         };
 
-        int id;
-        std::string name;
-        int capacity;
-        struct FacilityStats stats;
-        //gen type and values for generating
-        GenType gen;
-        double a;
-        double b;        
-        std::queue<ProcInQueue> q; //queue of Process IDs that wish to enter
+        int id;             ///< The ID of the facility
+        std::string name;   ///< The name of the facility
+        int capacity;       ///< The capacity of the facility
+        GenType gen;        ///< The generation type for facility usage time
+        double a;           ///< The first parameter for generating facility usage time (depends on generation type)
+        double b;           ///< The second parameter for generating facility usage time (depends on generation type)
+        struct FacilityStats stats; ///< The Facility statistics
+        std::queue<ProcInQueue> q;  ///< Queue of processes waiting to enter the facility
 
         double generateTime();
     };
 
-
-
+    /**
+     * @brief The Simulation class represents a discrete event simulation
+     * 
+     * Simulation manages the simulation time, event calendar, processes, and facilities
+     */
     class Simulation 
     {
     private:
-        double time;
-        double endTime;
+        double time;    ///< Current simulation time
+        double endTime; ///< End time of the simulation, -1 if the simulation should not end on timer
     public:
-        std::priority_queue<Event> calendar;
-        // std::shared_ptr<Simulation> sharedThis;
-        std::unordered_map<int, Process> procMap;
-        std::unordered_map<int, Facility> facMap;
+        std::priority_queue<Event> calendar;        ///< Priority queue for storing simulation events
+        std::unordered_map<int, Process> procMap;   ///< Map of processes in the simulation
+        std::unordered_map<int, Facility> facMap;   ///< Map of facilities in the simulation
+
 
         Simulation();
 
@@ -139,8 +173,8 @@ extern const bool Verbose; // Declare the extern variable
         double getTime ();
         void setEndTime(double time);
 
-        // void addEvent(int processID, int processNextState, int facilityID, double startTime, int priority, double timeCreated);
-        // void addEvent(Event e);
+        void addEvent(int processID, int processNextState, int facilityID, double startTime, int priority, double timeCreated);
+        void addEvent(Event e);
         void addProcessEvent(int processID, int processNextState, double startTime, int priority, double timeCreated);
         void addFacilityEvent(int processID, int processNextState, int facilityID, double startTime, int priority, double timeCreated);
         Event nextEvent();
@@ -167,6 +201,6 @@ extern const bool Verbose; // Declare the extern variable
 
 // } // namespace
 
-#endif
+#endif // DISCRETE_SIM_HPP
 
     
